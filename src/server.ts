1b54e45e7c1d7172f5263912feaec82beec36dad
DaniSan <danielsantoscarneiro0508@gmail.com>
2021-06-15 12:05:39 -0300

    inserindo cors no projeto

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors'
 import routes from './routes/routes';
 import path from 'path';
 
 const PORT = process.env.PORT || 3333;
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 app.use(routes);
 
